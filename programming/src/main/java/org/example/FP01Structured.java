508350abe952a3c79c4ff598e30f5ecefe09b862
Stan <stanislas.sekelj@gmail.com>
2026-01-04 13:03:20 +0000

    Exercice 1 - Odd numbers

--- programming/src/main/java/org/example/FP01Structured.java
+++ programming/src/main/java/org/example/FP01Structured.java
@@ -8,7 +8,8 @@ public class FP01Structured {
     public static void main(String[] args) {
         List<Integer> numbers = List.of(12,3,14,21,45,1,2,12);
         //printAllNumbersInListStructured(numbers);
-        printEvenNumbersInListFunctional(numbers);
+        //printEvenNumbersInListFunctional(numbers);
+        printOddNumbersInListFunctional(numbers);
     }
 
     public static void printAllNumbersInListStructured(List<Integer> list){
@@ -19,6 +20,10 @@ public class FP01Structured {
 
     public static void printEvenNumbersInListFunctional(List<Integer> list){
         list.stream().filter(number -> number % 2 == 0).forEach(System.out::println);
+    }
+
+    public static void printOddNumbersInListFunctional(List<Integer> list){
+        list.stream().filter(number -> !(number % 2 == 0)).forEach(System.out::println);
     }
 
     public static void printAllNumbersInListFunctional(List<Integer> list){
