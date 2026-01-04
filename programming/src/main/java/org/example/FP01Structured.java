7f110651875e334e054c9e28adab06e230c6e7cf
Stan <stanislas.sekelj@gmail.com>
2026-01-04 13:16:35 +0000

    exercice 2 - Print all courses individually

--- programming/src/main/java/org/example/FP01Structured.java
+++ programming/src/main/java/org/example/FP01Structured.java
@@ -7,19 +7,23 @@ public class FP01Structured {
 
     public static void main(String[] args) {
         List<Integer> numbers = List.of(12,3,14,21,45,1,2,12);
+        List<String> courses = List.of("Spring","Spring Boot","API","Microservices","AWS","Azure", "Docker", "Kubernetes");
         //printAllNumbersInListStructured(numbers);
         //printEvenNumbersInListFunctional(numbers);
-        printOddNumbersInListFunctional(numbers);
+        //printOddNumbersInListFunctional(numbers);
+        printCoursesInListFunctional(courses);
     }
 
     public static void printAllNumbersInListStructured(List<Integer> list){
-        for (Integer number : list) {
-            System.out.println(number);
-        }
+        for (Integer number : list) { System.out.println(number); }
     }
 
     public static void printEvenNumbersInListFunctional(List<Integer> list){
         list.stream().filter(number -> number % 2 == 0).forEach(System.out::println);
+    }
+
+    public static void printCoursesInListFunctional(List<String> list){
+        list.stream().forEach(System.out::println);
     }
 
     public static void printOddNumbersInListFunctional(List<Integer> list){
